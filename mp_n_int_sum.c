fb420bb9711d13f323c6d6da44496d73b9e81a7c
OsandaH <osandahiripitiya@gmail.com>
2025-03-07 06:26:29 +0530

    Update mp_n_int_sum.c

--- mp_n_int_sum.c
+++ mp_n_int_sum.c
@@ -1,63 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <math.h>
-
-#define N_THREADS 4
-
-int sum = 0;
-pthread_mutex_t mutex;
-
-typedef struct {
-    int start;
-    int end;
-} Range;
-
-void* calculate_sum(void* arg) {
-    Range* range = (Range*) arg;
-    int local_sum = 0;
-
-    for (int i = range->start; i <= range->end; i++) {
-        local_sum += i;
-    }
-
-    pthread_mutex_lock(&mutex);
-    sum += local_sum;
-    pthread_mutex_unlock(&mutex);
-
-    return NULL;
-}
+#include <omp.h>
 
 int main() {
-    int n;
-    printf("Enter the number of integers to sum: ");
-    scanf("%d", &n);
-
-    pthread_t threads[N_THREADS];
-    Range ranges[N_THREADS];
-
-    pthread_mutex_init(&mutex, NULL);
-
-    int segment_size = (int) ceil((double) n / N_THREADS);
-
-    for (int i = 0; i < N_THREADS; i++) {
-        ranges[i].start = i * segment_size + 1;
-        ranges[i].end = (i + 1) * segment_size;
-
-        if (ranges[i].end > n) {
-            ranges[i].end = n;
+    int n = 1000;
+    int sum = 0;
+    #pragma omp parallel
+    {
+        int thread_sum = 0;
+        #pragma omp for
+        for (int i = 1; i <= n; i++) {
+            thread_sum += i;
         }
-
-        pthread_create(&threads[i], NULL, calculate_sum, &ranges[i]);
+        #pragma omp critical
+        sum += thread_sum;
     }
-
-    for (int i = 0; i < N_THREADS; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    pthread_mutex_destroy(&mutex);
-
-    printf("The sum of the first %d integers is %d.\n", n, sum);
-
+    printf("The sum of the first %d integers is %d\n", n, sum);
     return 0;
 }
